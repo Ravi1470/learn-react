b2506fa1aa0384bd680eefaea5e7dff4c3ec4fd2
Ravi1470 <vk8580318@gmail.com>
2025-09-17 22:29:19 +0530

    disign completed

--- counterApp/src/App.jsx
+++ counterApp/src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [data, setData] = useState(0);
-
   const increment = () => {
     setData(data + 1);
     console.log(data);
@@ -17,23 +17,25 @@ const App = () => {
     setData(0);
   };
   return (
-    <div>
+    <div className="main">
       <h1>{data}</h1>
-      <button
-        className="inc"
-        onClick={increment}>
-        +1
-      </button>
-      <button
-        className="reset"
-        onClick={reset}>
-        reset
-      </button>
-      <button
-        className="dec"
-        onClick={decrement}>
-        -1
-      </button>
+      <div className="btncontainer">
+        <button
+          className="inc"
+          onClick={increment}>
+          +1
+        </button>
+        <button
+          className="reset"
+          onClick={reset}>
+          reset
+        </button>
+        <button
+          className="dec"
+          onClick={decrement}>
+          -1
+        </button>
+      </div>
     </div>
   );
 };
